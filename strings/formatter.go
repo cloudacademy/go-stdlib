058670788cd1d0bb279b182a16f99b4f37fb4e55
Jeremy Cook <jeremy.cook@cloudacademy.com>
2022-01-05 13:10:41 +1300

    updated var name

--- strings/formatter.go
+++ strings/formatter.go
@@ -5,14 +5,14 @@ import (
 	"math"
 )
 
-type Rocket struct{
-	Name string
+type Rocket struct {
+	Name   string
 	Engine string
 	Thrust float32
 	Length int
 }
 
-func main(){
+func main() {
 	//https://pkg.go.dev/fmt
 
 	var1 := 3
@@ -34,17 +34,17 @@ func main(){
 	fmt.Printf("item3 %[3]f, item2 %[2]s, item1 %[1]d\n", arr[0], arr[1], arr[2])
 	fmt.Printf("item3 %[3]T, item2 %[2]T, item1 %[1]T\n", arr[0], arr[1], arr[2])
 
-	v2 := Rocket{
-		Name: "saturnV",
+	saturnV := Rocket{
+		Name:   "saturnV",
 		Engine: "F-1",
 		Thrust: 1.5,
 		Length: 42,
 	}
 
-	fmt.Printf("%v\n", v2)
-	fmt.Printf("%+v\n", v2)
-	fmt.Printf("%T\n", v2)
+	fmt.Printf("%v\n", saturnV)
+	fmt.Printf("%+v\n", saturnV)
+	fmt.Printf("%T\n", saturnV)
 
-	var5 := fmt.Sprintf("area of circle radius 4 = %f", 2 * var3 * 4)
+	var5 := fmt.Sprintf("area of circle radius 4 = %f", 2*var3*4)
 	fmt.Println(var5)
 }
